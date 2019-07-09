3c380edb94129445e4c1ffea64af9ce0cde52b82
longnt <longnt1@kaopiz.com>
2019-07-09 10:16:14 +0700

    Grouping node for there purpose, added force result decorator

---
+++ Assets/Script/BehaviourTree/Decorator/BTForceResult.cs
@@ -0,0 +1,28 @@
+﻿namespace Kultie.BTs
+{
+    /// <summary>
+    /// Use this decorator for force result status of a node for desired result.
+    /// </summary>
+    public class BTForceResult : BTNode
+    {
+
+        BTNode node;
+        TreeNodeStatus desiredStatus;
+
+        public BTForceResult(BTNode _node,TreeNodeStatus _desiredStatus)
+        {
+            node = _node;
+            desiredStatus = _desiredStatus;
+        }
+
+        public override TreeNodeStatus Update(float dt)
+        {
+            _nodeStatus = node.Update(dt);
+            if(_nodeStatus == TreeNodeStatus.RUNNING){
+                return _nodeStatus;
+            }
+            _nodeStatus = desiredStatus;
+            return _nodeStatus;
+        }
+    }
+}
