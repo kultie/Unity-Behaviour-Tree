0e3f459f6b7b388c252c80e67089daaeeb4d2007
longnt <longnt1@kaopiz.com>
2019-10-03 17:17:18 +0700

    Remove redundant codes

--- Assets/Scripts/Base/BehaviourContext.cs
+++ Assets/Scripts/Base/BehaviourContext.cs
@@ -2,15 +2,4 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BehaviourContext : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-}
+public class BehaviourContext { }
